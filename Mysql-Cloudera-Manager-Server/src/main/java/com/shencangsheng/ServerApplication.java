60673d42d70556a5ddec57435eeaee79662c8298
jayden <liushiyu>
2021-09-22 18:24:30 +0800

    feat: gRpc

---
+++ Mysql-Cloudera-Manager-Server/src/main/java/com/shencangsheng/ServerApplication.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: ServerApplication
+ * Author:   shencangsheng
+ * Date:     2021/9/22 6:12 下午
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.shencangsheng;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author shencangsheng
+ * @create 2021/9/22
+ * @since 1.0.0
+ */
+
+public class ServerApplication {
+
+}
